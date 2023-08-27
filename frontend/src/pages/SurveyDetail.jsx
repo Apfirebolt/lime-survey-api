7a3892d76992e02cb71afd8d920bdff4206b5093
Apfirebolt <aspper20@gmail.com>
2023-08-27 21:05:08 +0530

    modal for adding question

--- frontend/src/pages/SurveyDetail.jsx
+++ frontend/src/pages/SurveyDetail.jsx
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import { useParams, useNavigate } from "react-router-dom";
+import AddQuestion from "../components/AddQuestion";
+import SurveyLogo from "../assets/survey-goals.jpg";
 
 import {
   updateSurvey,
@@ -16,7 +18,15 @@ const SurveyDetail = () => {
   const params = useParams();
   const navigate = useNavigate();
   const [toastMessage, setToastMessage] = useState("");
-  
+  const [isQuestionModalOpen, setIsQuestionModalOpened] = useState(false);
+
+  const closeQuestionModal = () => {
+    setIsQuestionModalOpened(false);
+  };
+
+  const openQuestionModal = () => {
+    setIsQuestionModalOpened(true);
+  };
 
   useEffect(() => {
     dispatch(getSurvey(params.id));
@@ -47,14 +57,7 @@ const SurveyDetail = () => {
       dispatch(resetVariables());
       navigate("/survey");
     }
-  }, [
-    dispatch,
-    isError,
-    isSuccess,
-    navigate,
-    message,
-    toastMessage,
-  ]);
+  }, [dispatch, isError, isSuccess, navigate, message, toastMessage]);
 
   const {
     register,
@@ -90,58 +93,80 @@ const SurveyDetail = () => {
   return (
     <form
       onSubmit={handleSubmit((data) => updateSurveyUtil(data))}
-      className="md:w-1/2 sm:w-3/4 mx-auto my-3"
+      className="sm:w-3/4 mx-auto my-3"
     >
-      <p className="text-center text-2xl my-3 text-red-700">Survey DETAIL</p>
-      <div className="mb-4">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="title"
-        >
-          Title
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="title"
-          type="text"
-          placeholder="Survey Title"
-          {...register("title", { required: true })}
-        />
-        {errors.title && <p className="text-red-500">Title is required.</p>}
+      <p className="text-center font-bold text-2xl my-8 text-gray-700">
+        SURVEY DETAIL
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="flex justify-center">
+          <img src={SurveyLogo} alt="logo" className="w-1/2" />
+        </div>
+
+        <div className="flex flex-col justify-center">
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="title"
+            >
+              Title
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="title"
+              type="text"
+              placeholder="Survey Title"
+              {...register("title", { required: true })}
+            />
+            {errors.title && <p className="text-red-500">Title is required.</p>}
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="description"
+            >
+              Description
+            </label>
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="description"
+              type="text"
+              placeholder="Survey Description"
+              rows="10"
+              {...register("description", { required: true })}
+            ></textarea>
+            {errors.description && (
+              <p className="text-red-500">Description is required.</p>
+            )}
+          </div>
+
+          <div>
+            <input
+              className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="submit"
+              value="Update Survey"
+            />
+            <button
+              onClick={(e) => deleteSurveyUtil(e)}
+              className="shadow mx-3 bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            >
+              Delete Survey
+            </button>
+            <button
+              type="button"
+              onClick={() => openQuestionModal()}
+              className="shadow mx-3 bg-gray-500 hover:bg-gray-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            >
+              Add Question
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="mb-4">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="description"
-        >
-          Description
-        </label>
-        <textarea
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="description"
-          type="text"
-          placeholder="Survey Description"
-          rows="10"
-          {...register("description", { required: true })}
-        ></textarea>
-        {errors.description && (
-          <p className="text-red-500">Description is required.</p>
-        )}
-      </div>
-      
-      <div>
-        <input
-          className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-          type="submit"
-          value="Update Survey"
-        />
-        <button
-          onClick={(e) => deleteSurveyUtil(e)}
-          className="shadow mx-3 bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-        >
-          Delete Survey
-        </button>
-      </div>
+      <AddQuestion
+        isOpen={isQuestionModalOpen}
+        openModal={openQuestionModal}
+        closeModal={closeQuestionModal}
+      />
     </form>
   );
 };
